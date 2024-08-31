1ff94a1a89c7fa7332ed2b5d9d6c0a266894d08e
gadjgasan1997 <gadj.gasan1997@gmail.com>
2024-08-31 23:40:38 +0300

    Реализовал пакет с утилитами для написания тестов

---
+++ src/Essentials.Cryptography.TestsUtils/Extensions/RsaCryptographyServiceFailMockExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Moq;
+using LanguageExt.Common;
+using Essentials.Cryptography.Services.RsaCryptographyService;
+
+namespace Essentials.Cryptography.TestsUtils.Extensions;
+
+/// <summary>
+/// Методы расширения для мока сервиса <see cref="IRsaCryptographyService" />, возвращающие ошибочный результат
+/// </summary>
+public static class RsaCryptographyServiceFailMockExtensions
+{
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRsaCryptographyService.GenerateKeyPairAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRsaCryptographyService> SetupFail_GenerateKeyPairAsync(
+        this Mock<IRsaCryptographyService> mock,
+        Error? error = null)
+    {
+        error ??= Error.New("Unknown Generate Key Pair Error");
+        return mock.Setup_GenerateKeyPairAsync(() => error);
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRsaCryptographyService.Decrypt" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="error">Требуемая ошибка</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRsaCryptographyService> SetupFail_Decrypt(
+        this Mock<IRsaCryptographyService> mock,
+        Error? error = null)
+    {
+        error ??= Error.New("Unknown Decrypt Error");
+        return mock.Setup_Decrypt((_, _) => error);
+    }
+}
---
+++ src/Essentials.Cryptography.TestsUtils/Extensions/RsaCryptographyServiceMockExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using LanguageExt;
+using LanguageExt.Common;
+using Essentials.Cryptography.Services.RsaCryptographyService;
+
+namespace Essentials.Cryptography.TestsUtils.Extensions;
+
+/// <summary>
+/// Методы расширения для мока сервиса <see cref="IRsaCryptographyService" />
+/// </summary>
+public static class RsaCryptographyServiceMockExtensions
+{
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRsaCryptographyService.GenerateKeyPairAsync" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRsaCryptographyService> Setup_GenerateKeyPairAsync(
+        this Mock<IRsaCryptographyService> mock,
+        Func<Validation<Error, (string PrivateKey, string PublicKey)>> func)
+    {
+        mock
+            .Setup(service => service.GenerateKeyPairAsync(It.IsAny<int>()))
+            .ReturnsAsync((int _) => func());
+        
+        return mock;
+    }
+    
+    /// <summary>
+    /// Устанавливает для мока поведение метода <see cref="IRsaCryptographyService.Decrypt" />
+    /// </summary>
+    /// <param name="mock">Мок</param>
+    /// <param name="func">Делегат, устанавливающий поведение для метода</param>
+    /// <returns>Мок</returns>
+    public static Mock<IRsaCryptographyService> Setup_Decrypt(
+        this Mock<IRsaCryptographyService> mock,
+        Func<string, string, Validation<Error, string>> func)
+    {
+        mock
+            .Setup(service =>
+                service.Decrypt(
+                    It.IsAny<string>(),
+                    It.IsAny<string>(),
+                    It.IsAny<int>(),
+                    It.IsAny<bool>()))
+            .Returns(
+                (
+                        string privateKey,
+                        string encryptedCodeWord,
+                        int _,
+                        bool _) =>
+                    func(privateKey, encryptedCodeWord));
+        
+        return mock;
+    }
+}
